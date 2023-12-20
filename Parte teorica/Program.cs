c8268cfdda38efa541c8a231583d5669593c21fb
daniel lucana cayo <daniellucanacayo@gmail.com>
2023-12-20 12:54:45 -0400

    comentario al actualizar

--- Parte teorica/Program.cs
+++ Parte teorica/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("La parte teorica está en la carpeta PDF,");
 Console.WriteLine("\n este texto es un cambio para prueba del git y ramas");
+Console.WriteLine("La parte teorica está en la carpeta PDF");
+//hola
+//hhh
+//kdhfkj
